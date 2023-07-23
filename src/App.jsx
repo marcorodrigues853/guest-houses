e7b0bed9dfd12152bcb5abda93c9deef1e677e52
Marco Rodrigues <marcoaurelio853@gmail.com>
2023-07-23 12:04:16 +0100

    style: StyledComponents defaults and conditionals

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,10 @@ import styled from "styled-components"
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
-
-const H1 = styled.h1`
-  font-size:30px;
-  font-weight:600;
-  background-color: red;
-`;
-
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: orangered;
   padding:20px;
 `;
 
@@ -20,15 +14,33 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>
-          Wild oasis
-        </H1>
-        <Button onClick={() => alert("check in ")}>Check in</Button>
-        <Button onClick={() => alert("check out ")}>Check out</Button>
-        <Input type="number" placeholder="Number of guests" />
-        <Input type="number" placeholder="Number of guests" />
 
-      </StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as='h1'>
+              Wild oasis
+            </Heading>
+            <div>
+              <Heading as='h2'>
+                Check in and out
+              </Heading>
+              <Button onClick={() => alert("check in ")}>Check in</Button>
+              <Button variation="danger" size="small" onClick={() => alert("check out ")}>Check out</Button>
+            </div>
+          </Row>
+
+
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form >
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+
+        </Row>
+
+      </StyledApp >
     </>
   )
 }
