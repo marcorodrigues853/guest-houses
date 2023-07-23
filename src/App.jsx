1780e5c4a53478083d506584b3c02c78fe00479f
Marco Rodrigues <marcoaurelio853@gmail.com>
2023-07-23 11:29:35 +0100

    style  global components and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-import { useState } from 'react'
+import styled from "styled-components"
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size:30px;
+  font-weight:600;
+  background-color: red;
+`;
 
 
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding:20px;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      'HELLO'
+      <GlobalStyles />
+      <StyledApp>
+        <H1>
+          Wild oasis
+        </H1>
+        <Button onClick={() => alert("check in ")}>Check in</Button>
+        <Button onClick={() => alert("check out ")}>Check out</Button>
+        <Input type="number" placeholder="Number of guests" />
+        <Input type="number" placeholder="Number of guests" />
+
+      </StyledApp>
     </>
   )
 }
