6cff93d7eddcf5c40561e807572d205ccbee7de4
Marco Rodrigues <marcoaurelio853@gmail.com>
2023-07-23 13:14:51 +0100

    style: do Row styled variants

--- src/ui/Row.jsx
+++ src/ui/Row.jsx
@@ -1,26 +1,25 @@
-import { css, styled } from "styled-components";
+import { css, styled } from 'styled-components'
 
 const Row = styled.div`
-display:flex;  
+  display: flex;
 
-${props => props.type === 'horizontal' && css`
-    justify-content:space-between;
-    align-items:center;
-`}
+  ${props =>
+    props.type === 'horizontal' &&
+    css`
+      justify-content: space-between;
+      align-items: center;
+    `}
 
-${props => props.type === 'vertical' && css`
-    flex-direction:column;
-    gap:1.6rem;
-`}
-
-
-`;
-
-
-
+  ${props =>
+    props.type === 'vertical' &&
+    css`
+      flex-direction: column;
+      gap: 1.6rem;
+    `}
+`
 
 Row.defaultProps = {
-    stype: 'vertical'
+  stype: 'vertical',
 }
 
-export default Row;
+export default Row
