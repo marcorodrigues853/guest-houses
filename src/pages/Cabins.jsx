3224fe37279bfa6e10ef641ee579a6a8820fe3d3
Marco Rodrigues <marcoaurelio853@gmail.com>
2023-07-23 16:32:37 +0100

    feat: display iamges

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,16 +1,25 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Heading from '../ui/Heading'
 import Row from '../ui/Row'
 import { getCabins } from '../services/apiCabins'
 
 function Cabins() {
+  const [cabins, setCabins] = useState([])
   useEffect(function () {
-    getCabins().then(data => console.log(data))
+    getCabins().then(data => {
+      console.log(data)
+      setCabins(data)
+    })
+
+    console.log(cabins)
   }, [])
+
   return (
     <Row type='horizontal'>
       <Heading as='h1'>All cabins</Heading>
-      <p>TEST</p>
+
+      {!cabins.length && <span>Loading...</span>}
+      {cabins.length > 0 && <img src={cabins[0].image} alt='' />}
     </Row>
   )
 }
