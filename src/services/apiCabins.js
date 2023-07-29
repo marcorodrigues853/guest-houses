4c7a8f531dba948572b34567f11a8805324ae784
Marco Rodrigues <marcoaurelio853@gmail.com>
2023-07-29 19:13:27 +0100

    core: add style and dele apiCabins

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,17 +1,22 @@
-import supabase from "./supabase";
+import supabase from './supabase'
 
 export async function getCabins() {
+  const { data, error } = await supabase.from('cabins').select('*')
 
-let { data, error } = await supabase
-.from('cabins')
-.select('*');
-
-
-if(error){
-    console.error('Cabins could not be loaded ', error);
+  if (error) {
+    console.error('Cabins could not be loaded ', error)
     throw new Error('Cabins could not be loaded')
+  }
+
+  return data
 }
 
-return data;
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from('cabins').delete().eq('id', id)
+  if (error) {
+    console.error(error)
+    throw new Error('Cabin could not be deleted')
+  }
 
+  return data
 }
